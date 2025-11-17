bf25d6d8e2aab3a3e8ada06b0ae3cd94406ef5f5
RobertoRodriguezCarbonell <98robers@gmail.com>
2025-11-17 13:09:29 +0100

    feat: implement dashboard layout with logout functionality and update password input type

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -132,7 +132,7 @@ export function LoginForm({
                     <FormItem>
                       <FormLabel>Password</FormLabel>
                       <FormControl>
-                        <Input placeholder="*********" {...field} />
+                        <Input type="password" placeholder="*********" {...field} />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
---
+++ components/logout.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { authClient } from "@/lib/auth-client"
+import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
+
+export function Logout() {
+    const handleLogout = async () => {
+        await authClient.signOut();
+        window.location.href = "/";
+    }
+
+    return (
+        <Button variant="destructive" onClick={handleLogout}>
+            Logout <LogOut className="size-4" />
+        </Button>
+    )
+}
